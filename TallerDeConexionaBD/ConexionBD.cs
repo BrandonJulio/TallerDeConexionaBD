13c097387cfb9101007ba430c8f201f47480866a
BrandonJulio <bjulio@unicesar.edu.co>
2020-12-03 03:54:20 -0500

    Agregar archivos de proyecto.

---
+++ TallerDeConexionaBD/ConexionBD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TallerDeConexionaBD
+{
+    class ConexionBD
+    {
+        string cadena = ConfigConnetion.connectionString;
+
+        public SqlConnection conectarbd = new SqlConnection();
+
+
+        public ConexionBD()
+        {
+
+            conectarbd.ConnectionString = cadena;
+        }
+
+        public string abrir()
+        {
+
+            try
+            {
+                string mensaje;
+                conectarbd.Open();
+                mensaje = "Conexion Abierta";
+                conectarbd.Close();
+                return mensaje;
+            }
+            catch (Exception ex)
+            {
+                return @"Error al abrir la base de datos : {ex.Message}";
+            }
+            finally { conectarbd.Close(); }
+
+        }
+        public void cerrar()
+        {
+
+            conectarbd.Close();
+
+        }
+    }
+}
---
+++ TallerDeConexionaBD/Principal.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TallerDeConexionaBD
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConectar = new System.Windows.Forms.Button();
+            this.lbMensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnConectar
+            // 
+            this.BtnConectar.Location = new System.Drawing.Point(202, 204);
+            this.BtnConectar.Name = "BtnConectar";
+            this.BtnConectar.Size = new System.Drawing.Size(101, 27);
+            this.BtnConectar.TabIndex = 0;
+            this.BtnConectar.Text = "Conectar";
+            this.BtnConectar.UseVisualStyleBackColor = true;
+            this.BtnConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbMensaje
+            // 
+            this.lbMensaje.AutoSize = true;
+            this.lbMensaje.Location = new System.Drawing.Point(56, 297);
+            this.lbMensaje.Name = "lbMensaje";
+            this.lbMensaje.Size = new System.Drawing.Size(90, 13);
+            this.lbMensaje.TabIndex = 1;
+            this.lbMensaje.Text = "Conexion cerrada";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.lbMensaje);
+            this.Controls.Add(this.BtnConectar);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConectar;
+        private System.Windows.Forms.Label lbMensaje;
+    }
+}
+
